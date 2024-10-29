f327949349b2389411822802386ee6384cbe853a
João Guilherme <joaoguilhermesg198@gmail.com>
2024-10-28 23:45:02 -0300

    Fiz as atribuições a classe Empresa

--- Aula02/src/Empresa.java
+++ Aula02/src/Empresa.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+
 public class Empresa {
-    
+    private int cnpj;
+    private String razaoSocial;
+    public String nome;
+    private ArrayList<NotaFiscal> notas;
 }
