908f5419233cc6d25edacada14e53ba208d918cc
João Guilherme <joaoguilhermesg198@gmail.com>
2024-10-28 23:25:52 -0300

    Preenchi a classe NotaFiscal, coloquei a array necessária, conectei com as outras classes

--- Aula02/src/NotaFiscal.java
+++ Aula02/src/NotaFiscal.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+
 public class NotaFiscal {
-    
+    public int numero;
+    public String data;
+    public float desconto;
+    private Cliente cliente;
+    private Entregador entregador;
+    private Empresa empresa;
+    public ArrayList<ItemNota> itens;
 }
